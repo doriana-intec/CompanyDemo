1aa92c397af3032d97ede4888e9f0441aa3c52ea
bartLaptopInDeKlas <bart.hunerbein@intecbrussel.be>
2020-03-09 10:22:43 +0100

    sql file

---
+++ src/main/resources/sql_file/Create_tables.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE company_db;
+USE company_db;
+
+CREATE TABLE companies
+(
+    company_id INT PRIMARY KEY AUTO_INCREMENT,
+    name       VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE employees
+(
+    employee_id INT PRIMARY KEY AUTO_INCREMENT,
+    name        VARCHAR(50) NOT NULL,
+    job_title   VARCHAR(50) NOT NULL,
+    company_id  INT         NOT NULL,
+    Foreign Key fk_company_id (company_id) REFERENCES companies (company_id)
+        ON UPDATE CASCADE
+        ON DELETE NO ACTION
+);
